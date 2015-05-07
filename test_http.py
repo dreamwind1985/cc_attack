063950322963c528eff7be9faa2b2c11f7b9916f
root <root@localhost.localdomain>
2015-05-07 22:03:19 +0800

    add

--- test_http.py
+++ test_http.py
@@ -1,21 +1,21 @@
 #!/usr/bin env python
 import socket
-host="www.haosou.com"
+host="10.18.31.79"
 se=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 se.connect((host,80))
 se.send("GET / HTTP/1.1\n")
-se.send("Accept:text/html,application/xhtml+xml,*/*;q=0.8\n")
+#se.send("Accept:text/html,application/xhtml+xml,*/*;q=0.8\n")
 #se.send("Accept-Encoding:gzip,deflate,sdch\n")
-se.send("Accept-Language:zh-CN,zh;q=0.8,en;q=0.6\n")
-se.send("Cache-Control:max-age=0\n")
-se.send("Connection:keep-alive\n")
-se.send("Host:"+host+"\r\n")
-se.send("Referer:http://www.haosou.com/\n")
-se.send("user-agent: Googlebot\n\n")
+#se.send("Accept-Language:zh-CN,zh;q=0.8,en;q=0.6\n")
+#se.send("Cache-Control:max-age=0\n")
+#se.send("Connection:keep-alive\n")
+se.send("Host:"+host+"\r\n\r\n")
+#se.send("Referer:http://www.haosou.com/\n")
+#se.send("user-agent: Googlebot\n\n")
 while True:
     buf = se.recv(1024)
     if not len(buf):
         break
     print buf
-    se.close()
-    break
+    #se.close()
+    #break
--- test_sock.py
+++ test_sock.py
@@ -6,16 +6,111 @@
 
 
 import socket
-
+import select
 def test_sock():
     socks = []
-    for i in range(999, -1, -1):
-        socks[i] = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        socks[i].setblocking(0)
-        socks[i].connect("10.18.31.79", 80)
-
-
-    rlist, wlist, elist = select.select(socks, socks, socks)
-    for sock in wlist:
-        sock.send()
+    rsocks = []
+    for i in range(0, 1000):
+        socks.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    for sock in socks:
+        sock.setblocking(0)
+        try:
+            sock.connect(("10.18.31.79", 80))
+        except:
+            pass
+    while(len(socks) or len(rsocks)):
+        rlist, wlist, elist = select.select(rsocks,socks, socks)
+        for sock in wlist:
+            #print sock.getpeername()
+            str = http_request()
+            try:
+                sock.send(str)
+            except:
+                pass
+            socks.remove(sock)
+            rsocks.append(sock)
     
+        for sock in rlist:
+            sock.close()
+            rsocks.remove(sock)
+        for sock in elist:
+            if sock in rsocks:
+                rsocks.remvoe(sock)
+            if sock in socks:
+                socks.remove(sock)
+            sock.close()
+        print(len(socks))
+        print(len(rsocks))
+
+def test_one():
+    socks = []
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setblocking(0)
+    try:
+        sock.connect(("10.18.31.79", 80))
+    except:
+        pass
+
+    wlist = select.select([],[sock], [])[1]
+    print wlist
+    for s in wlist:
+        print sock
+        if s == sock:
+            print 123
+            peername = s.getpeername()
+        else:
+            continue
+
+    sock.close()
+
+
+def test_poll():
+    socks = []
+    rsocks = []
+    epoll = select.epoll()
+    conn_q = {}
+    read_q = {}
+    request = http_request()
+    for i in range(0, 1000):
+        socks.append(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
+    for sock in socks:
+        sock.setblocking(0)
+        try:
+            sock.connect(("10.18.31.79", 80))
+        except:
+            pass
+        epoll.register(sock.fileno(), select.EPOLLOUT)
+        conn_q[sock.fileno()] = sock
+
+    while(len(conn_q) or len(read_q)):
+        events = epoll.poll(1)
+        for fileno, event in events:
+            if conn_q.has_key(fileno):
+                if event & select.EPOLLOUT:
+                    conn_q[fileno].send(request)
+                    epoll.modify(fileno, select.EPOLLIN)
+                    read_q[fileno] = conn_q[fileno]
+                    del conn_q[fileno]
+                else:
+                    print "except"
+                    del conn_q[fileno]
+            if read_q.has_key(fileno):
+                if event & select.EPOLLIN:
+                    epoll.modify(fileno,0)
+                    read_q[fileno].close()
+                    del read_q[fileno]
+                else:
+                    pass
+            else:
+                print "error"
+                pass
+
+def http_request():
+    return "GET / HTTP/1.1\nHost:10.18.31.79\r\n\r\n" 
+
+
+if __name__=="__main__":
+    #test_sock()
+    #test_one()
+    test_poll()
+
